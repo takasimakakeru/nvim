d0f9b8f4f138922756d3875765ee90d0f78acb45
takasimakakeru <kyunpangoogle@outlook.jp>
2026-07-01 19:29:57 +0900

    Refactor lazy.nvim setup and key mappings
    Updated lazy.nvim installation and configuration settings.

--- init.lua
+++ init.lua
@@ -1,25 +1,19 @@
--- lazy.nvim の自動インストール（ここは前と同じです）
+-- 1. lazy.nvim の自動インストール
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- 基本設定
+-- 2. リーダーキーの設定
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- 3. 外部設定ファイルの読み込み
+require("config.lazy")
+require("config.lsp")
+
 vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-
--- ⭕【ここがポイント】
--- 「"plugins"」とだけ指定すると、lua/plugins/ フォルダの中にある
--- すべての .lua ファイルを自動でスキャンして、プラグインとして読み込んでくれます！
-require("lazy").setup("plugins")
-
--- HTMLの自作マクロ
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "html", "xml", "javascriptreact", "typescriptreact", "vue" },
-  callback = function()
-    vim.keymap.set("i", ">", "><Esc>F<lyt>o</<C-r>\"><Esc>O", { buffer = true, silent = true })
-  end,
-})
